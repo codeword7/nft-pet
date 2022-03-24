342a77fc198f97891c22ec4afa4e0fb9a59b6ae9
Neeraj <neerajk@somnoware.com>
2022-03-24 21:57:34 +0530

    📦 NEW: add basic setup

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const mnemonic = process.env.MNEMONIC.toString().trim()
+
+module.exports = {
+
+   networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://matic-mumbai.chainstacklabs.com`,
+        ),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  },
+
+};

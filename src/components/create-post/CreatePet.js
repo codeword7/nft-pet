2a76b020fa8e06c96647a6af0c025dec0c22ff7f
Neeraj <neerajk@somnoware.com>
2022-03-26 10:46:14 +0530

    📦 NEW: add createPet function

--- src/components/create-post/CreatePet.js
+++ src/components/create-post/CreatePet.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import PhotoCamera from '@material-ui/icons/PhotoCamera'
-import './CreatePet.css'
+import React, { useState, createRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import './CreatePet.css';
 import {
   TextField,
   Container,
@@ -10,14 +10,46 @@ import {
   Button,
   IconButton,
   MenuItem,
-} from '@material-ui/core'
+} from '@material-ui/core';
+import { NFTStorage, File } from 'nft.storage';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQ0MEIwYTZiNmFCYjUyOTc5OGRmNmI1MGJDYzc4NEI0YkZCQWJjNGQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0ODI2OTY5MTEzOSwibmFtZSI6IlBldE5mdCJ9.Go994cM-6Msw8WzjQ29c7iwU3mNd1wbiFnHuH15jr2Q';
 
 function CreatePet() {
   // Add variables
+  const history = useHistory();
+  const [image, setImage] = useState('');
+  const petTypeRef = createRef();
+  const [petName, setPetName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [ownerName, setOwnerName] = useState('');
+  const [imageName, setImageName] = useState('');
+  const [imageType, setImageType] = useState('');
+  const [petType, setPetType] = useState('');
 
-  const handleImage = (event) => {}
+  const handleImage = (event) => {
+    setImage(event.target.files[0]);
+    setImageName(event.target.files[0].name);
+    setImageType(event.target.files[0].type);
+  };
 
-  const handleSubmit = async (e) => {}
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const client = new NFTStorage({ token: API_KEY });
+      const metadata = await client.store({
+        name: petName,
+        description: `${ownerName}, ${petType}`,
+        image: new File([image], imageName, { type: imageType }),
+      });
+      if (metadata) {
+        history.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   return (
     <StylesProvider injectFirst>
@@ -29,14 +61,79 @@ function CreatePet() {
           <Typography className="title" color="textPrimary" gutterBottom>
             Add a photo of your pet
           </Typography>
-          
+
           {/* Add Form */}
-
-
+          {
+            image ? (
+              <img src={URL.createObjectURL(image)} alt="pet" className="img-preview" />
+            ) : (
+              ''
+            )
+          }
+          <div className="form-container">
+            <form className="form" noValidate autoComplete="off">
+              <input
+                accept="image/*"
+                className="input"
+                id="icon-button-photo"
+                defaultValue={image}
+                onChange={handleImage}
+                type="file"
+              />
+              <label htmlFor="icon-button-photo">
+                <IconButton color="primary" component="span">
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Pet's name"
+                variant="outlined"
+                className="text-field"
+                defaultValue={petName}
+                onChange={(e) => setPetName(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="Owner's name"
+                variant="outlined"
+                className="text-field"
+                defaultValue={ownerName}
+                onChange={(e) => setOwnerName(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                name="petType"
+                select
+                label="Choose one option"
+                variant="outlined"
+                className="text-field"
+                onChange={(e) => setPetType(e.target.value)}
+                defaultValue=""
+                ref={petTypeRef}
+              >
+                <MenuItem value="Cat">Cat</MenuItem>
+                <MenuItem value="Dog">Dog</MenuItem>
+                <MenuItem value="Bird">Bird</MenuItem>
+                <MenuItem value="Fish">Fish</MenuItem>
+                <MenuItem value="Other">Other</MenuItem>
+              </TextField>
+              <Button
+                size="large"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </form>
+          </div>
         </div>
       </Container>
     </StylesProvider>
-  )
+  );
 }
 
-export default CreatePet
+export default CreatePet;
